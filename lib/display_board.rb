cd0039844dbf6d0ccb027b6249cf73390a4cbe81
John Wisneski <john.j.wisneski@gmail.com>
2020-10-24 04:51:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,5 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "#{board[0]}   |#{board[1]}   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
